eee2101e10553be652b54142472a50a53079d580
Ihor-Slastonov <104766756+Ihor-Slastonov@users.noreply.github.com>
2022-10-22 22:20:19 +0300

    almost done)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,73 @@
-
+import SimpleLightbox from "simplelightbox";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import PixabayApiService from './js-service/pixabay-service.js'
 import markupGallery from './templates/card.hbs';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = {
     gallery: document.querySelector('.js-gallery'),
     form: document.querySelector('.js-search-form'),
-    loadMoreBtn: document.querySelector('.js-btn-more'),
+    guard: document.querySelector('.guard'),
 };
-// refs.loadMoreBtn.setAttribute('hidden','true')
-const pixabayApiService = new PixabayApiService();
 
+const pixabayApiService = new PixabayApiService();
+// ------ Infinity scroll ------
+const options = {
+    root: null,
+    rootMargin: '200px',
+    threshold: 1,
+}
+const observer = new IntersectionObserver(onLoad, options);
+// ----------------------------
 
 refs.form.addEventListener('submit', onSearchSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
 
 function onSearchSubmit(e) {
     e.preventDefault();
     clearGallery();
-    pixabayApiService.query = refs.form.elements.searchQuery.value;
-    pixabayApiService.resetPage()
-    pixabayApiService.fetchPictures().then(appendCardsMarkup);
+    pixabayApiService.query = refs.form.elements.searchQuery.value.trim();
+    if (pixabayApiService.query === '') {
+        return Notify.failure('It cannot be empty field')
+    }
+    pixabayApiService.resetPage();
+    pixabayApiService.fetchPictures().then(checkAndAppendMarkup);
+    e.currentTarget.reset();
 }
+
+
+function checkAndAppendMarkup(resultPromise) {
+    if (resultPromise.hits.length != 0) {
+        Notify.success(`Hooray! We found ${resultPromise.totalHits} images.`);
+        appendCardsMarkup(resultPromise);
+        observer.observe(refs.guard);
+    } else {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
+}
+// ----- настрока infinity scroll ----//
+function onLoad(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            onLoadMore();
+        }
+    })
+}
+//-----------------------------------//
 
 function onLoadMore() {
-    pixabayApiService.fetchPictures().then(appendCardsMarkup);
+    let currentPage = pixabayApiService.currentPage;
+    let totalPages = pixabayApiService.allPages;
+    if (currentPage > totalPages) {
+        Notify.failure("We're sorry, but you've reached the end of search results.");
+        observer.unobserve(refs.guard)
+    } else {
+        return pixabayApiService.fetchPictures().then(appendCardsMarkup);
+    }
 }
-
 function appendCardsMarkup(card) {
     refs.gallery.insertAdjacentHTML('beforeend', markupGallery(card))
 }
-
 function clearGallery() {
     refs.gallery.innerHTML = '';
 }
--- src/js-service/pixabay-service.js
+++ src/js-service/pixabay-service.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '30754633-49a4360a1de811b7fe1cb3ed9';
@@ -8,6 +7,8 @@ export default class PixabayApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
+        this.perPage = 40;
+        this.totalPages = 0;
     }
     async fetchPictures() {
         try {
@@ -19,14 +20,14 @@ export default class PixabayApiService {
                     orientation: 'horizontal',
                     safesearch: 'true',
                     page: this.page,
-                    per_page: 40,
+                    per_page: this.perPage,
                 }
             });
-            this.page += 1;
-            if (response.data.hits.length === 0) {
-                Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            }
-            return await response.data;
+            this.incrementPage();
+            const data = await response.data;
+            let pages = Math.ceil(data.totalHits / data.hits.length);
+            this.totalPages = pages;
+            return data
         } catch (error) {
             console.log(error);
         }
@@ -35,12 +36,29 @@ export default class PixabayApiService {
     resetPage() {
         this.page = 1;
     }
-
+    incrementPage() {
+        this.page += 1;
+    }
     get query() {
         return this.searchQuery;
     }
 
     set query(newQuery) {
         this.searchQuery = newQuery;
+    }
+
+    get currentPage() {
+        return this.page
+    }
+
+    set currentPage(value) {
+        this.page = value;
+    }
+
+    get allPages() {
+        return this.totalPages;
+    }
+    set allPages(value) {
+        this.totalPages = value;
     }
 }
