e8628410301d09b7bbaf667d559ca50a4e68721e
Ihor-Slastonov <jammik000@gmail.com>
2022-10-23 21:56:12 +0300

    back to top btn

--- src/index.js
+++ src/index.js
@@ -49,14 +49,7 @@ function checkAndAppendMarkup(resultPromise) {
         Notify.success(`Hooray! We found ${resultPromise.totalHits} images.`);
         appendCardsMarkup(resultPromise);
         observer.observe(refs.guard);
-        SmoothScroll({
-            stepSize: 175,
-            animationTime: 800,
-            accelerationDelta: 200,
-            accelerationMax: 6,
-            keyboardSupport: true,
-            arrowScroll: 100,
-        });
+        smoothScroll()
     } else {
         Notify.failure("Sorry, there are no images matching your search query. Please try again.")
         return
@@ -95,4 +88,36 @@ function clearGallery() {
     refs.gallery.innerHTML = '';
     SmoothScroll.destroy()
 }
+function smoothScroll() {
+    SmoothScroll({
+        stepSize: 175,
+        animationTime: 800,
+        accelerationDelta: 200,
+        accelerationMax: 6,
+        keyboardSupport: true,
+        arrowScroll: 100,
+    });
+}
 
+const toTopBtn = document.querySelector('.back-to-top');
+toTopBtn.addEventListener('click', backToTop);
+window.addEventListener('scroll', onScroll);
+
+function onScroll() {
+    const scrolledValue = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+    if (scrolledValue > coords) {
+        toTopBtn.classList.add('back-to-top--show')
+    }
+    if (scrolledValue < coords) {
+        toTopBtn.classList.remove('back-to-top--show')
+    }
+
+}
+
+function backToTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+    }
+}
